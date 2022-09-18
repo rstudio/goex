57545a44eb1278e3aac173479276bb2a24af3020
Dan Buch <dan@meatballhat.com>
2022-09-17 20:55:10 -0400

    Add more tests around ptr.From and zero values

--- ptr/ptr_test.go
+++ ptr/ptr_test.go
@@ -38,20 +38,51 @@ func TestFrom(t *testing.T) {
 		{[]int{4, 2}, []int{4, 2}},
 		{map[string]bool{"chomp": true}, map[string]bool{"chomp": true}},
 		{struct{ banjo string }{banjo: "kazooie"}, struct{ banjo string }{banjo: "kazooie"}},
-		// NOTE: figure out how to do this (if it's even possible ?)
-		// {(*string)(nil), ""},
-		// {(*bool)(nil), false},
-		// {(*int)(nil), 0},
-		// {(*uint)(nil), uint(0)},
-		// {(*float64)(nil), float64(0)},
-		// {(*[]int)(nil), []int{}},
-		// {(*map[string]bool)(nil), map[string]bool{}},
-		// {(*struct{ banjo string })(nil), struct{ banjo string }{}},
 	} {
 		t.Run(fmt.Sprintf("%[1]T", tc.in), func(t *testing.T) {
 			require.Equal(t, tc.out, ptr.From(ptr.To(tc.in)))
 		})
 	}
+
+	t.Run("*string to zero", func(t *testing.T) {
+		var v *string
+		require.Equal(t, "", ptr.From(v))
+	})
+
+	t.Run("*bool to zero", func(t *testing.T) {
+		var v *bool
+		require.Equal(t, false, ptr.From(v))
+	})
+
+	t.Run("*int to zero", func(t *testing.T) {
+		var v *int
+		require.Equal(t, 0, ptr.From(v))
+	})
+
+	t.Run("*uint to zero", func(t *testing.T) {
+		var v *uint
+		require.Equal(t, uint(0), ptr.From(v))
+	})
+
+	t.Run("*float64 to zero", func(t *testing.T) {
+		var v *float64
+		require.Equal(t, float64(0), ptr.From(v))
+	})
+
+	t.Run("*[]int to zero", func(t *testing.T) {
+		var v *[]int
+		require.Equal(t, ([]int)(nil), ptr.From(v))
+	})
+
+	t.Run("*map[string]bool to zero", func(t *testing.T) {
+		var v *map[string]bool
+		require.Equal(t, (map[string]bool)(nil), ptr.From(v))
+	})
+
+	t.Run("*struct to zero", func(t *testing.T) {
+		var v *struct{ banjo string }
+		require.Equal(t, struct{ banjo string }{}, ptr.From(v))
+	})
 }
 
 func TestRoundTrip(t *testing.T) {
